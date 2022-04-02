ffe2ebdd349dc683d00c485ee5838e559cc30d03
Berkan Yıldırım <berkanyldrim@gmail.com>
2022-04-02 12:50:06 +0300

    Commit1: Kullanılan temanın assets klasörü import edildi. App.vue ayarlandı ve ilk component oluşturuldu.

---
+++ src/components/HeaderAndBanner.vue
@@ -0,0 +1,7 @@
+<template>
+    <!---header-and-banner-section-->
+      <div class="header-and-banner-con w-100 float-left position-relative">
+         <div class="header-and-banner-inner-con">
+         </div>
+      </div>
+</template>
