dadb7d36c58ff314a115a2c41b24c5cb89dc6961
Berkan Yıldırım <berkanyldrim@gmail.com>
2022-04-03 15:53:16 +0300

    Commit4:Modallar eklendi içerikleri düzenlenmedi.

--- src/components/MainBody.vue
+++ src/components/MainBody.vue
@@ -2,96 +2,104 @@
     <!-- Main Body Start -->
     <section class="w-100 float-left service-con padding-top padding-bottom position-relative" id="service-con" >
         <div class="container">
-        <div class="service-inner-con position-relative">
-            
-            <div class="generic-title text-center">
-                <h6>My Expertise</h6>
-                <h2 class="mb-0">Provide Wide Range of<br>
-                    Digital Services
-                </h2>
-            </div>
-            <div class="service-box wow fadeInUp">
-                <div class="row">
-                    <!-- Education Area Start -->
-                    <div class="col-lg-6 col-md-6">
-                    <div class="service-box-item">
-                        <figure class="mb-0">
-                            <img src="assets/image/service-icon1.png" alt="service-icon" class="img-fluid">
-                        </figure>
-                        <div class="service-box-item-content">
-                            <h4>Education</h4>
-                            <p>Dolor repellendus temporibus autem 
-                                quibusdam officiis debitis rerum neces
-                                aibus minima veniam.
-                            </p>
-                            <a href="#" data-toggle="modal" data-target="#Ui-Design">Read More</a>
-                        </div>
-                    </div>
-                    </div>
-                    <!-- Education Area End -->
-
-                    <!-- Experience Area Start -->
-                    <div class="col-lg-6 col-md-6">
-                    <div class="service-box-item">
-                        <figure class="mb-0">
-                            <img src="assets/image/service-icon2.png" alt="service-icon" class="img-fluid">
-                        </figure>
-                        <div class="service-box-item-content">
-                            <h4>Professional Experience </h4>
-                            <p>Dolor repellendus temporibus autem 
-                                quibusdam officiis debitis rerum neces
-                                aibus minima veniam.
-                            </p>
-                            <a href="#" data-toggle="modal" data-target="#web-design">Read More</a>
-                        </div>
-                    </div>
-                    </div>
-                    <!-- Experience Area End -->
+            <div class="service-inner-con position-relative">
+                
+                <div class="generic-title text-center">
+                    <h6>About</h6>
+                    <h2 class="mb-0">Brief Summary of <br>
+                        My Life
+                    </h2>
                 </div>
-                <div class="row mb-0">
-                    <!-- Course Area Start -->
-                    <div class="col-lg-6 col-md-6">
-                    <div class="service-box-item">
-                        <figure class="mb-0">
-                            <img src="assets/image/service-icon3.png" alt="service-icon" class="img-fluid">
-                        </figure>
-                        <div class="service-box-item-content">
-                            <h4>Courses and Certificates</h4>
-                            <p>Dolor repellendus temporibus autem 
-                                quibusdam officiis debitis rerum neces
-                                aibus minima veniam.
-                            </p>
-                            <a href="#" data-toggle="modal" data-target="#web-development">Read More</a>
+                <div class="service-box wow fadeInUp">
+                    <div class="row">
+                        <!-- Education Area Start -->
+                        <div class="col-lg-6 col-md-6">
+                        <div class="service-box-item">
+                            <figure class="mb-0">
+                                <img src="assets/image/service-icon1.png" alt="service-icon" class="img-fluid">
+                            </figure>
+                            <div class="service-box-item-content">
+                                <h4>Education</h4>
+                                <p>My education life
+                                </p>
+                                <a href="#" data-toggle="modal" data-target="#Education">Read More</a>
+                            </div>
                         </div>
-                    </div>
-                    </div>
-                    <!-- Course Area End -->
+                        </div>
+                        <!-- Education Area End -->
 
-                    <!-- More information Area Start -->
-                    <div class="col-lg-6 col-md-6">
-                    <div class="service-box-item mb-0">
-                        <figure class="mb-0">
-                            <img src="assets/image/service-icon4.png" alt="service-icon" class="img-fluid">
-                        </figure>
-                        <div class="service-box-item-content">
-                            <h4>More Information</h4>
-                            <p>Hobbies , areas of interest ,personal characteristics 
-                            </p>
-                            <a href="#" data-toggle="modal" data-target="#app-development">Read More</a>
+                        <!-- Experience Area Start -->
+                        <div class="col-lg-6 col-md-6">
+                        <div class="service-box-item">
+                            <figure class="mb-0">
+                                <img src="assets/image/service-icon2.png" alt="service-icon" class="img-fluid">
+                            </figure>
+                            <div class="service-box-item-content">
+                                <h4>Professional Experience </h4>
+                                <p>Information about my professional experience
+                                </p>
+                                <a href="#" data-toggle="modal" data-target="#Experience">Read More</a>
+                            </div>
                         </div>
+                        </div>
+                        <!-- Experience Area End -->
                     </div>
+                    <div class="row mb-0">
+                        <!-- Course Area Start -->
+                        <div class="col-lg-6 col-md-6">
+                        <div class="service-box-item">
+                            <figure class="mb-0">
+                                <img src="assets/image/service-icon3.png" alt="service-icon" class="img-fluid">
+                            </figure>
+                            <div class="service-box-item-content">
+                                <h4>Courses and Certificates</h4>
+                                <p>Courses I have attended and certificates I have received
+                                </p>
+                                <a href="#" data-toggle="modal" data-target="#Course">Read More</a>
+                            </div>
+                        </div>
+                        </div>
+                        <!-- Course Area End -->
+
+                        <!-- More information Area Start -->
+                        <div class="col-lg-6 col-md-6">
+                        <div class="service-box-item mb-0">
+                            <figure class="mb-0">
+                                <img src="assets/image/service-icon4.png" alt="service-icon" class="img-fluid">
+                            </figure>
+                            <div class="service-box-item-content">
+                                <h4>More Information</h4>
+                                <p>Hobbies , areas of interest ,personal characteristics 
+                                </p>
+                                <a href="#" data-toggle="modal" data-target="#Information">Read More</a>
+                            </div>
+                        </div>
+                        </div>
+                        <!-- More information Area End -->
                     </div>
-                    <!-- More information Area End -->
                 </div>
             </div>
-        </div>
+            <EducationModal></EducationModal>
+            <ExperienceModal></ExperienceModal>
+            <CourseAndCertificateModal></CourseAndCertificateModal>
+            <MoreInformationModal></MoreInformationModal>
         </div>
     </section>
     <!-- Main Body Start -->
 </template>
 
 <script>
+import EducationModal from  "./ModalsComponents/EducationModal.vue"
+import ExperienceModal from "./ModalsComponents/ExperienceModal.vue"
+import CourseAndCertificateModal from "./ModalsComponents/CourseAndCertificateModal.vue"
+import MoreInformationModal from "./ModalsComponents/MoreInformationModal.vue"
 export default {
-    name:'MainBody'
+    name:'MainBody',
+    components: {
+    EducationModal,
+    ExperienceModal,
+    CourseAndCertificateModal,
+    MoreInformationModal
+}
 }
 </script>
---
+++ src/components/ModalsComponents/CourseAndCertificateModal.vue
@@ -0,0 +1,30 @@
+<template>
+<div id="Course" class="modal fade" tabindex="-1" style="display: none;" aria-hidden="true">
+    <div class="modal-dialog">
+    <div class="modal-content">
+        <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true" ><i class="far fa-times"></i></span></button>
+        </div>
+        <div class="modal-body service-model-content">
+            <figure class="mb-0">
+                <img src="assets/image/web-development-model-img.jpg" alt="web-development-model-img" class="img-fluid">
+            </figure>
+            <h4>Web Development</h4>
+            <p class="mb-md-4 mb-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>  
+                <ul class="list-unstyled model-list mb-md-3 mb-2">
+                <li><i class="fas fa-check-circle"></i> Lorem Ipsum is simply dummy text of the printing and typesetting industry</li>
+                <li><i class="fas fa-check-circle"></i> Lorem Ipsum is simply dummy text of the printing and typesetting industry</li>
+                <li><i class="fas fa-check-circle"></i> Lorem Ipsum is simply dummy text of the printing and typesetting industry</li>
+            </ul>
+            <p class="mb-0">Lorem Ipsum is simply dummy text of the printing and typesetting industry. when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>  
+        </div>
+    </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default{
+    name:"CourseAndCertificateModal"
+}
+</script>
